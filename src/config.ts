5c581a91b92f25b7b9b1a40ff2baf57a5d39de5d
Zhan9YunHua <zy.hua1122@qq.com>
2018-09-17 18:10:52 +0800

    token todo

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,12 @@
 export default {
   mongodb: 'mongodb://zyhuatest:test@118.24.103.174:27017/webtest',
-  prot: 1123
+  prot: 1123,
+  timeout: 7200,
+  tokenPayload: {
+    iss: 'zyhua.cn',
+    admin: 'zyhua',
+    sub: 'mange',
+    auth: 'Bearer',
+    exp: 1122
+  }
 }
