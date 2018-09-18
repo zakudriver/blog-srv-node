99f09e0aa0fb4e2512df9bd25ec317bf70af91dd
Zhan9YunHua <zy.hua1122@qq.com>
2018-09-18 17:57:59 +0800

    dev daily

--- test.ts
+++ test.ts
@@ -24,16 +24,39 @@
 // // console.log(new Tes().nam)
 // console.log((Tes as any).cc)
 
-var b = () => {
-  console.log('b')
-}
-var a: any = () => {
-  console.log('a')
-}
-a = [a]
-a.splice(0, 0, b)
+// var b = () => {
+//   console.log('b')
+// }
+// var a: any = () => {
+//   console.log('a')
+// }
+// a = [a]
+// a.splice(0, 0, b)
 
+// console.log(a)
+const dosm: any = {
+  get: (target: any, key: any) => {
+    console.log(target);
+    console.log(key);
+    return target[key];
+  },
+  set: (target: any, key: any, val: any) => {
+    console.log(target);
+    console.log(key);
+    console.log(val);
+    target[key] = val;
+    return true
+  }
+};
 
-console.log(a)
+const t: any = new Proxy(
+  {
+    name: 'test',
+    aget: 222
+  },
+  dosm
+);
 
-
+// console.log(t.name);
+t.aget = 11;
+// console.log(t);
