b2766d7c1b3d23c091a0246a9271506f8c9d44ea
Kumo Tyou <zy.hua1122@qq.com>
2018-10-21 22:56:13 +0800

    test => req transmit

--- src/controller/message.controller.ts
+++ src/controller/message.controller.ts
@@ -2,6 +2,8 @@ import * as Koa from 'koa';
 import { prefix, router, log, required, auth } from '../middleware/router/decorators';
 import { MessageMod } from '../db/model';
 import { trycatch } from '../libs/utils';
+import * as http from 'http';
+import { http_request } from '../libs/promisify';
 
 @prefix('/message')
 export default class MessageController {
@@ -48,20 +50,51 @@ export default class MessageController {
   async addMessage(ctx: Koa.Context) {
     // const req = ctx.request.body as { email: string; text: string; uid: string };
     // req.uid = ctx.request.uid;
+    const options = {
+      hostname: '116.62.201.8',
+      port: 5002,
+      path: '/webapi/Home/NewsList',
+      method: 'GET'
+    };
 
-    const newMessage = new MessageMod({ email: '11111', text: '2222', uid: ctx.request.uid });
-    await trycatch(
-      ctx,
-      async () => {
-        await newMessage.save();
-        ctx.body = {
-          code: 0,
-          data: null,
-          msg: 'message sent successfully'
-        };
-      },
-      'message sent failed'
-    );
+    function Pro(): Promise<string> {
+      return new Promise(resolve => {
+        const req = http.request(options, res => {
+          let resp = '';
+          res.setEncoding('utf8');
+          res.on('data', chunk => {
+            console.log(`响应主体: ${chunk}`);
+            resp += chunk;
+          });
+          res.on('end', () => {
+            resolve(resp);
+          });
+        });
+
+        req.on('error', (err: any) => {
+          console.log(err);
+        });
+
+        req.end(() => console.log('over'));
+      });
+    }
+
+    const res = await Pro();
+    ctx.body = JSON.parse(res);
+
+    // const newMessage = new MessageMod({ email: '11111', text: '2222', uid: ctx.request.uid });
+    // await trycatch(
+    //   ctx,
+    //   async () => {
+    //     await newMessage.save();
+    //     ctx.body = {
+    //       code: 0,
+    //       data: null,
+    //       msg: 'message sent successfully'
+    //     };
+    //   },
+    //   'message sent failed'
+    // );
   }
 
   @router({
--- src/libs/promisify.ts
+++ src/libs/promisify.ts
@@ -1,8 +1,11 @@
 import * as util from 'util';
 import * as fs from 'fs';
+import * as http from 'http';
 
 export const fs_stat = util.promisify(fs.stat);
 
 export const fs_readdir = util.promisify(fs.readdir);
 
 export const fs_unlink = util.promisify(fs.unlink);
+
+export const http_request = util.promisify(http.request);
