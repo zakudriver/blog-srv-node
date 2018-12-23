b73f2640390ddb686e450e9098575052278e089b
Kumo Tyou <zy.hua1122@qq.com>
2018-12-23 22:56:21 +0800

    message date format

--- src/middleware/socket/message.event.ts
+++ src/middleware/socket/message.event.ts
@@ -1,5 +1,4 @@
-import * as Router from 'koa-router';
-import * as Koa from 'koa';
+import * as moment from 'moment';
 import { Event } from '../../constants/enum';
 import MessageMod, { IMessage } from '../../db/model/message';
 import { sucLog } from '../../libs/log';
@@ -12,15 +11,15 @@ export function onMessage(io: SocketIO) {
   });
 }
 
-export async function emitMessage(io: SocketIO, msg?: IMessage) {
-  let emitData: IMessage[] = [];
-  if (msg) {
-    emitData.push(msg);
-  } else {
-    emitData = await MessageMod.find({ isRead: false }, { article: 1, name: 1, time: 1, text: 1, _id: 1 }).populate(
-      'article',
-      'title'
-    );
-  }
+export async function emitMessage(io: SocketIO) {
+  const emitData = await MessageMod.find({ isRead: false }, { article: 1, name: 1, time: 1, text: 1, _id: 1 })
+    .populate('article', 'title')
+    .lean();
+
+  emitData.forEach((i: IMessage) => {
+    i.time = moment(i.time).format('YYYY-MM-DD HH:mm:ss');
+    i.text = i.text.substr(0, 10);
+  });
+
   io.emit(Event.Message, emitData);
 }
